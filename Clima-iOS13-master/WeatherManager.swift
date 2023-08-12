350c1b08243b404fc929d07c9b1f3fe12d3f5cce
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-08-12 14:20:19 +0200

    using closure and creating weather types

--- Clima-iOS13-master/WeatherManager.swift
+++ Clima-iOS13-master/WeatherManager.swift
@@ -24,16 +24,37 @@ struct WeatherManager {
             let session = URLSession(configuration: .default)
             
             //3. Give th session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            //            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!) // can force unwrap because we already checked if it is not nil
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                    
+                }
+            }
             
             //4. Start the task
             task.resume() // called resume and not start becuase they start of in a suspended state
         }
-        
-        
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        }
+        catch {
+            print(error)
+        }
     }
     
     func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        //        keeping for reference
         if error != nil {
             print(error!) // can force unwrap because we already checked if it is not nil
             return
@@ -42,7 +63,7 @@ struct WeatherManager {
         if let safeData = data {
             // tough to print responses we have to first convert to a string
             let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+            print(dataString ?? "")
         }
     }
 }
