20b240c2805de55afb5fc2de6f7c652186191309
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-08-14 20:57:39 +0200

    refactoring to use extensions

--- Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -25,6 +25,23 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         searchTextField.delegate = self // super forgettable
     }
 
+    
+    
+//    func didUpdateWeather(weather: WeatherModel) {
+//        print("Did update weateher\(weather)")
+//    }
+    
+//    func didUpdateWeather(weatherManager: WeatherManager, weather: WeatherModel) {
+//        print("Did update weateher\(weather)")
+//    }
+    
+    
+    
+    
+}
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true) // we are manually putting the textfield on end editing mode
         print(searchTextField.text!)
@@ -38,7 +55,7 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         // normally used to perform some validation
         if textField.text != "" {
-            return true // let the end editing 
+            return true // let the end editing
         } else {
             textField.placeholder = "Type something"
             return false // keep user in editing mode
@@ -52,15 +69,11 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         }
         searchTextField.text = "" // saves us doing it twice after line 26 or after line 31
     }
-    
-//    func didUpdateWeather(weather: WeatherModel) {
-//        print("Did update weateher\(weather)")
-//    }
-    
-//    func didUpdateWeather(weatherManager: WeatherManager, weather: WeatherModel) {
-//        print("Did update weateher\(weather)")
-//    }
-    
+}
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
     // Following Apples naming convention
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
 //        temperatureLabel.text = weather.temperatureString // UILabel.text must be used from main thread only
@@ -81,7 +94,87 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     func didFailWithError(error: Error) {
         print(error)
     }
-    
-    
 }
 
+
+
+
+// What it was like before refatroing to use extensions
+//import UIKit
+//
+//
+//class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+//
+//    @IBOutlet weak var conditionImageView: UIImageView!
+//    @IBOutlet weak var temperatureLabel: UILabel!
+//    @IBOutlet weak var cityLabel: UILabel!
+//    @IBOutlet weak var searchTextField: UITextField!
+//
+//    var weatherManager = WeatherManager()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        weatherManager.delegate = self // this is sooo forgettable
+//        searchTextField.delegate = self // super forgettable
+//    }
+//
+//    @IBAction func searchPressed(_ sender: UIButton) {
+//        searchTextField.endEditing(true) // we are manually putting the textfield on end editing mode
+//        print(searchTextField.text!)
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        searchTextField.endEditing(true) // end etiting and dismiss the keyboard
+//        print(searchTextField.text!)
+//        return true
+//    }
+//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        // normally used to perform some validation
+//        if textField.text != "" {
+//            return true // let the end editing
+//        } else {
+//            textField.placeholder = "Type something"
+//            return false // keep user in editing mode
+//        }
+//
+//    }
+//
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        if let city = searchTextField.text {
+//            weatherManager.fetchWeather(cityName: city)
+//        }
+//        searchTextField.text = "" // saves us doing it twice after line 26 or after line 31
+//    }
+//
+////    func didUpdateWeather(weather: WeatherModel) {
+////        print("Did update weateher\(weather)")
+////    }
+//
+////    func didUpdateWeather(weatherManager: WeatherManager, weather: WeatherModel) {
+////        print("Did update weateher\(weather)")
+////    }
+//
+//    // Following Apples naming convention
+//    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+////        temperatureLabel.text = weather.temperatureString // UILabel.text must be used from main thread only
+////        print("Did update weateher\(weather)")
+//
+////        DispatchQueue.main.async { [self] in
+////            temperatureLabel.text = weather.temperatureString
+////            conditionImageView.image = UIImage(systemName: weather.conditionName)
+////        }
+//
+//
+//        DispatchQueue.main.async {
+//            self.temperatureLabel.text = weather.temperatureString
+//            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+//        }
+//    }
+//
+//    func didFailWithError(error: Error) {
+//        print(error)
+//    }
+//
+//
+//}
