8e280653824e584a0d6a7881e2ad0aa15fde11c7
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-14 15:42:20 +0200

    seguing to next view controller

--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculateViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculateViewController: UIViewController {
 
+    var bmiValue = "0.0"
+    
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     
@@ -34,12 +36,28 @@ class ViewController: UIViewController {
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightSlider.value
         let weight = weightSlider.value
-        let bmi = weight / pow(height, 2)
         
+        let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%.1f", bmi)
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
+//        goToProgramaticViewController(bmi: bmi)
+        
+        print(bmi)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC =  segue.destination as! ResultsViewController
+            destinationVC.bmiValue = bmiValue
+        }
+    }
+    
+    func goToProgramaticViewController(bmi: Float) {
         let secondViewController = SecondViewController()
         secondViewController.bmiValue = String(format: "%.1f", bmi)
         self.present(secondViewController, animated: true, completion: nil)
-        print(bmi)
     }
     
     
