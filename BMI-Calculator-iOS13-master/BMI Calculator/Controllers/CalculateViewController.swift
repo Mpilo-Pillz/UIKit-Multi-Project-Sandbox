098ce6d648c861d5a68f8955aa395fb120ce3342
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-15 09:24:00 +0200

    completing bmi calculator

--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculateViewController.swift
@@ -55,6 +55,8 @@ class CalculateViewController: UIViewController {
             let destinationVC =  segue.destination as! ResultsViewController
 //            destinationVC.bmiValue = bmiValue
             destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
         }
     }
     
--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,28 +6,48 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var bmiValue = "0.0"
-    var bmi: Float? = nil
+//    var bmiValue = "0.0"
+    var bmi: BMI?
     
-   mutating func calculateBMIMisolution(height: Float, weight: Float){
-        let bmi = weight / pow(height, 2)
-        bmiValue = String(format: "%.1f", bmi)
-    }
-    
-    func getBMIValueMysolution() -> String {
-        return self.bmiValue
-    }
+//   mutating func calculateBMIMisolution(height: Float, weight: Float){
+//        let bmi = weight / pow(height, 2)
+//        bmiValue = String(format: "%.1f", bmi)
+//    }
+//
+//    func getBMIValueMysolution() -> String {
+//        return self.bmiValue
+//    }
     
     func getBMIValue() -> String {
-        let bmiToDecimalPlace = String(format: "%.1f", bmi!)
+        let bmiToDecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiToDecimalPlace
     }
     
+    func getAdvice() -> String {
+        return bmi?.advise ?? "No BMI information" // wont hit this but best to catch it ncase of a refactor and we forget
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.systemBackground
+    }
+    
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            print("Underweight")
+            bmi = BMI(value: bmiValue, advise: "Eat more snacks", color: UIColor.systemBlue)
+        } else if bmiValue < 24.9{
+            bmi = BMI(value: bmiValue, advise: "You are perfectly fit", color: UIColor.systemGreen)
+            print("Normal weight")
+        } else {
+            bmi = BMI(value: bmiValue, advise: "Eat less snacks", color: UIColor.systemRed)
+            print("Overweight")
+        }
+
     }
     
 }
