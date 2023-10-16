d2810cb989fb2375714ed491187ce7df18a75518
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-16 03:08:39 +0200

    starting egg timer

---
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+//    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle ?? "Medium"
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        case "Hard":
+//            print(hardTime)
+//        default:
+//            print("Error, this button is not suppsoed to be hooked to hardness")
+//        }
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
+}
