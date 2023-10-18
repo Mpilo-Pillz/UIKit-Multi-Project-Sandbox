30e8518dc3ebabbb7b12530943c721baed542f03
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-18 07:18:50 +0200

    adding story functionality

--- Destini-LayoutPractice-iOS13-master/Destini-iOS13/Controller/ViewController.swift
+++ Destini-LayoutPractice-iOS13-master/Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyNumber = 0
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyNumber += 1
+        print("choice \(storyNumber)")
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories[storyNumber].title
+        choice1Button.setTitle(stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(stories[storyNumber].choice2, for: .normal)
+    }
+    
 }
 
