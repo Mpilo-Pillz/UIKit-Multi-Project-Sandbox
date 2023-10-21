9ac12e2384727bdf45bb2f8d9a390c2c7e3474c9
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-21 16:21:19 +0200

    creating and using constants

---
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Mpilo Pillz on 2023/10/21.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+// The static keyword chages it form being a instance property to a value property
+// Meaning no need to create an object out of Constance to be able to use the proeprties
+import Foundation
+
+struct K {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    let plate = "plate"
+}
+
+
