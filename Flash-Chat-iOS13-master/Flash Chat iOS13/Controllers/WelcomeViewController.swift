b52a49717d9ccf7ecee44a322904ea7d8317e051
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-22 16:24:47 +0200

    hidng nave bar for welcoem screen

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -13,6 +13,18 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel! //UILabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated) // so the parent rund its code
+        // hiding it before the welcome co troller shows
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated) //so the superclass runs in built in tasks
+        // unhiding is so that it is visible for the other screens
+        // after thi voew conttroller disappears
+        navigationController?.isNavigationBarHidden = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
