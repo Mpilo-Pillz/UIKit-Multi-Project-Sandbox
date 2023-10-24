76b2440a6ef2c0c87eee0526cc891c6afa4b0245
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-24 20:04:31 +0200

    completeing excercise

--- Tipsy-iOS13-LayoutPractice-master/Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy-iOS13-LayoutPractice-master/Tipsy/Controllers/CalculatorViewController.swift
@@ -15,18 +15,64 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
+    
+    var tipAmount = 0.10
+    var numberOfPeople = 2
+    var billPerPerson = 0.0
+    var finalResult = "0.0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = !sender.isSelected
+        let buttonTitle = sender.currentTitle!
+        
+        let tipAmountWithoutPercent = buttonTitle.dropLast()
+        tipAmount = Double(tipAmountWithoutPercent)! / 100
+        
+        
+        print(buttonTitle)
         
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        numberOfPeople = Int(sender.value)
+        splitNumberLabel.text = String(numberOfPeople)
+       
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let totalBill = Double(billTextField.text!)! // TODO refactor
+        
+        if billTextField.text! != "" {
+            billPerPerson = (totalBill + (totalBill * tipAmount) ) / Double(numberOfPeople)
+            let billInTwoDecimal = String(format: "%.2f", billPerPerson)
+            finalResult = billInTwoDecimal
+            print(finalResult)
+        } else {
+            print("show Error and disable button")
+            
+        }
+        
+        self.performSegue(withIdentifier: "showCalculationResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showCalculationResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tipAmount * 100)
+            destinationVC.split = numberOfPeople
+        }
     }
 }
 
--- Tipsy-iOS13-LayoutPractice-master/Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy-iOS13-LayoutPractice-master/Tipsy/Controllers/ResultsViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
     }
     
 
@@ -26,5 +34,8 @@ class ResultsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func recalculatePRessed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
