84d31aafb92953e4b5988875565bd4fb234ef954
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-25 22:09:29 +0200

    notes to understand app deleage

--- Todoey-iOS13-master/Todoey/AppDelegate.swift
+++ Todoey-iOS13-master/Todoey/AppDelegate.swift
@@ -16,15 +16,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("did dinishe launching with options, when the app first loads up happens before view did load")
+        
+        // where the file will be stored for the simulators saved local storage and th einfo plist (the sand box)
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        print("Triggerd when something happens to the app eg if they get a call do something here to save the data")
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        print("right ehrn the app enter the background form foreground")
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -38,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+        print("right when the app is terminated by user or system eg to save resources")
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
