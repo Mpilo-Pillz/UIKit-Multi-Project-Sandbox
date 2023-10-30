799be8e14c19a98720cbfcd1ea503e7b46e890d9
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-10-30 06:10:38 +0200

    creating coredata playground

---
+++ CoreDataDojo/CoreDataDojo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDojo
+//
+//  Created by Mpilo Pillz on 2023/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
