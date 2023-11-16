48f2fb0496ecc0cb0e0c10d62eee27702be307f6
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-11-16 18:49:28 +0200

    Adding image picker and reading form camera

---
+++ SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Mpilo Pillz on 2023/11/16.
+//
+
+import UIKit
+import CoreML
+import Vision // helps us process images easily without too much code
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true // let the user crop the image working on less area for the subject
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // optional bind
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
